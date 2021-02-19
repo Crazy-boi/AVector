d2b872a6dcff3cd8ed549eb56406951d0b79f77f
Crazy-boi <legolas3579@gmail.com>
2021-02-19 20:54:46 +0100

    Update base methods, add methods

--- AVector.js
+++ AVector.js
@@ -4,37 +4,52 @@ function AVector(x, y) {
 }
 
 AVector.prototype = {
-    add: function(v, a="n") {
+    angle: function(inDegrees=false) {
+        let a;
+        a = Math.atan(this.y / this.x);
+        if (this.x < 0)
+            a += Math.PI;
+        if (a < 0)
+            a = 2*Math.PI + a;
+        if (typeof inDegrees == "boolean" && inDegrees)
+            return a/2/Math.PI*360;
+        return a;
+    },
+    length: function() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    },
+
+    add: function(v, a="sub_y") {
         if (v instanceof AVector) {
             this.x += v.x;
             this.y += v.y;
-        } else {
+        } else if (typeof v == "number") {
             if (typeof a == "number") {
                 this.x += v;
                 this.y += a;
-            } else if (typeof v == "number") {
+            } else {
                 this.x += v;
                 this.y += v;
             }
         }
         return this;
     },
-    sub: function(v, a="n") {
+    sub: function(v, a="sub_y") {
         if (v instanceof AVector) {
             this.x -= v.x;
             this.y -= v.y;
-        } else {
+        } else if (typeof v == "number") {
             if (typeof a == "number") {
                 this.x -= v;
                 this.y -= a;
-            } else if (typeof v == "number") {
+            } else {
                 this.x -= v;
                 this.y -= v;
             }
         }
         return this;
     },
-    mult: function(v, a="x") {
+    mult: function(v, a="mult_y") {
         if (typeof v == "number") {
             if (typeof a == "number") {
                 this.x *= v;
@@ -46,7 +61,7 @@ AVector.prototype = {
         }
         return this;
     },
-    div: function(v, a="x") {
+    div: function(v, a="div_y") {
         if (typeof v == "number") {
             if (typeof a == "number") {
                 this.x /= v;
@@ -57,12 +72,33 @@ AVector.prototype = {
             }
         }
         return this;
+    },
+
+    normalize: function() {
+        let a = this.length();
+        this.x /= a;
+        this.y /= a;
+        return this;
+    },
+    limit: function(a) {
+        if (typeof a == "number" && this.length() > a) {
+            this.normalize();
+            this.mult(a);
+        }
+        return this;
+    },
+
+
+
+    clone: function() {
+        return new AVector(this.x, this.y);
     }
 }
 
 // Test
 let v = new AVector(1, 1);
 
-v.mult(5, 2);
-
 write(v);
+
+//write(v.mult(5, 3));
+//write(v.angle(true));
--- write.js
+++ write.js
@@ -1,14 +1,26 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 700;
+canvas.width = 400;
 canvas.height = 300;
 
-ctx.font = "25px Arial";
+const fs = 20;
+let y = 2*fs;
+ctx.font = fs + "px Arial";
 
 function write(s) {
+    ctx.fillStyle = "black";
     if (s instanceof AVector) {
-        ctx.fillStyle = "black";
-        ctx.fillText("(" + s.x + "," + s.y + ")", 10, 50);
-    }
+        ctx.fillText("(" + r(s.x) + ", " + r(s.y) + ")", 10, y);
+        console.log("(" + s.x + ", " + s.y + ")");
+    } else if (typeof s == "number") {
+        ctx.fillText("" + r(s), 10, y);
+        console.log(s);
+    } else return;
+    y += 2*fs;
+}
+
+function r(n) {
+    let d = 4;
+    return Math.round(n * Math.pow(10, d)) / Math.pow(10, d);
 }
